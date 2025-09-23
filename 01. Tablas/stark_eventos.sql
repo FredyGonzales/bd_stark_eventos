d1da7beea1624306a941b78b3782902b1736b8c6
FredyGonzales <fredy.gonzales@interseguro.com.pe>
2025-09-23 10:32:34 -0500

    RegistroCumpleañosCliente

---
+++ 01. Tablas/stark_eventos.sql
@@ -0,0 +1,23 @@
+-- stark_eventos.stark_eventos definition
+
+CREATE TABLE `stark_eventos` (
+  `id_evento` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_anfitrion` varchar(100) DEFAULT NULL,
+  `email_anfitrion` varchar(100) DEFAULT NULL,
+  `contacto_anfitrion` varchar(100) DEFAULT NULL,
+  `nombre_cumpleaniero` varchar(100) DEFAULT NULL,
+  `edad_cumple` int(11) DEFAULT NULL,
+  `fecha_evento` datetime DEFAULT NULL,
+  `hora_inicio` varchar(20) DEFAULT NULL,
+  `hora_fin` varchar(20) DEFAULT NULL,
+  `direccion_exacta` text DEFAULT NULL,
+  `numero_invitados` int(11) DEFAULT NULL,
+  `id_tema_cumpleanos` varchar(100) DEFAULT NULL,
+  `comentarios_evento` text DEFAULT NULL,
+  `aud_usr_registro` varchar(24) DEFAULT NULL,
+  `aud_fec_registro` datetime DEFAULT NULL,
+  `aud_usr_modificacion` varchar(24) DEFAULT NULL,
+  `aud_fec_modificacion` datetime DEFAULT NULL,
+  `estado_evento` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_evento`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
